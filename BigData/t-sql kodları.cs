6e8c3abcf29e94fe457f7f21390eb84dfc7bf6a2
durukanEsmeray <durukanesmeray@gmail.com>
2023-03-29 09:39:57 +0300

    T-SQL kodları. Oluşturulan tablo ve proc lar mevcut.

---
+++ BigData/t-sql kodları.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+create database BuyukDatalarIleCalismak
+go 
+use BuyukDatalarIleCalismak
+go 
+create table Kisiler
+(
+ID int identity(1,1) primary key,
+Isim nvarchar(30),
+Soyisim nvarchar(30),
+EmailAdres nvarchar(60),
+TelefonNumarasi nvarchar(20),
+Resim varbinary(max)
+)
+go
+create proc KisiEkle
+(
+@Isim nvarchar(30),
+@Soyisim nvarchar(30),
+@EmailAdres nvarchar(60),
+@TelefonNumarasi nvarchar(20),
+@Resim varbinary(max)
+)
+as
+begin
+insert into Kisiler (Isim, Soyisim, EmailAdres, TelefonNumarasi, Resim)
+values(@Isim, @Soyisim, @EmailAdres, @TelefonNumarasi, @Resim)
+end
